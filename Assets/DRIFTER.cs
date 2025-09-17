e486e6f019eb3d0df04262d972b6250cd0e78a93
LordBlubbee <125277444+LordBlubbee@users.noreply.github.com>
2025-09-18 00:16:49 +0200

    Experimentation with Rooms

---
+++ Assets/DRIFTER.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class DRIFTER : MonoBehaviour
+{
+    public SPACE Interior;
+}
--- Assets/SCR/CAM.cs
+++ Assets/SCR/CAM.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class CAM : MonoBehaviour
 {
     public static CAM cam;
-    public Camera camera;
+    public Camera camob;
     private void Awake()
     {
         cam = this;
@@ -16,8 +16,8 @@ public class CAM : MonoBehaviour
         if (LOCALCO.local.GetPlayer() == null) return;
 
         CameraPosMain = LOCALCO.local.GetPlayer().transform.position;
-        Vector3 Offset = (camera.ScreenToViewportPoint(Input.mousePosition) - new Vector3(0.5f, 0.5f));
-        transform.position = CameraPosMain + new Vector3(Offset.x*30f, Offset.y*20f);
+        Vector3 Offset = (camob.ScreenToViewportPoint(Input.mousePosition) - new Vector3(0.5f, 0.5f));
+        transform.position = CameraPosMain + new Vector3(Offset.x*3f * camob.orthographicSize, Offset.y*2f * camob.orthographicSize);
         transform.position = new Vector3(transform.position.x, transform.position.y, -100);
     }
 }
